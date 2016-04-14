2d7059520b0ce83ace29f9c7c3a9f0eb77f2f773
David Yeung <quxbaz@gmail.com>
2016-04-13 23:57:23 -0400

    Removed util.uniqId()

--- tests/util.js
+++ tests/util.js
@@ -1,12 +1,7 @@
 import * as util from 'trax/lib/util'
 
 describe("util", () => {
-  describe("uniqid()", () => {
-    it("Creates 10000 unique ids.", () => {
-      let ids = new Set()
-      for (let i=0; i < 10000; i++)
-        ids.add(util.uniqId())
-      ids.size.should.eql(10000)
-    })
-  })
+  it("no tests yet", () => {
+
+  });
 })
