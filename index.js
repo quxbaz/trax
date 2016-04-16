4964570d5457d0befec393de9e6e05022feee264
David Yeung <quxbaz@gmail.com>
2016-04-16 00:51:42 -0400

    Removed dead commented code

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import sequencer from './lib/sequencer'
 import channel from './lib/channel'
 import blip from './lib/blip'
+import AudioService from './lib/AudioService'
 
-// import Channel from './lib/channel'
-// import Blip from './lib/blip'
-// import AudioService from './lib/audioservice'
-
-// export {Sequencer, Channel, Blip, AudioService}
-export {sequencer, channel, blip}
+export {sequencer, channel, blip, AudioService}
