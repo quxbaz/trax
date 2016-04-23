5ffe0d4edd98f2a7433ce525ea79215dd0e99e36
David Yeung <quxbaz@gmail.com>
2016-04-23 13:31:05 -0400

    setBlipAt

--- tests/tests/channels.js
+++ tests/tests/channels.js
@@ -20,12 +20,9 @@ describe("channel", () => {
       ).toEqual(stateAfter)
     })
 
-    it("Creates a bare channel.", () => {
-      const action = {
-        type: channels.actionTypes.CREATE_CHANNEL,
-        payload: {id: 'foo'}
-      }
+    it("Creates a channel.", () => {
       const stateBefore = undefined
+      const action = channels.actions.createChannel({id: 'foo'})
       const stateAfter = {
         foo: {
           ...initialState,
@@ -37,96 +34,96 @@ describe("channel", () => {
       ).toEqual(stateAfter)
     })
 
-    it("Creates a channel with child blips.", () => {
+    // it("Creates a channel with child blips.", () => {
 
-      // Mock uuid function to generate predictable ids
-      const uuid = util.uuid
-      let i = 0
-      util.uuid = () => i++
+    //   // Mock uuid function to generate predictable ids
+    //   const uuid = util.uuid
+    //   let i = 0
+    //   util.uuid = () => i++
 
-      const store = createStore(
-        combineReducers({
-          channels: channels.reducer,
-          blips: blips.reducer
-        }),
-        applyMiddleware(thunk)
-      )
+    //   const store = createStore(
+    //     combineReducers({
+    //       channels: channels.reducer,
+    //       blips: blips.reducer
+    //     }),
+    //     applyMiddleware(thunk)
+    //   )
 
-      const action = channels.actions.createChannel({id: 'foo'})
-      const stateAfter = {
-        foo: {
-          ...initialState,
-          id: 'foo',
-          blips: _.range(16)
-        }
-      }
+    //   const action = channels.actions.createChannel({id: 'foo'})
+    //   const stateAfter = {
+    //     foo: {
+    //       ...initialState,
+    //       id: 'foo',
+    //       blips: _.range(16)
+    //     }
+    //   }
 
-      store.dispatch(action)
+    //   store.dispatch(action)
 
-      expect(
-        store.getState().channels
-      ).toEqual(stateAfter)
+    //   expect(
+    //     store.getState().channels
+    //   ).toEqual(stateAfter)
 
-      expect(
-        Object.keys(store.getState().blips)
-      ).toEqual(_.range(16).map(i => i.toString()))
+    //   expect(
+    //     Object.keys(store.getState().blips)
+    //   ).toEqual(_.range(16).map(i => i.toString()))
 
-      expect(
-        _.values(store.getState().blips).map(
-          state => state.id
-        )
-      ).toEqual(_.range(16))
+    //   expect(
+    //     _.values(store.getState().blips).map(
+    //       state => state.id
+    //     )
+    //   ).toEqual(_.range(16))
 
-      const storeBlips = store.getState().blips
-      _.keys(storeBlips).forEach((key) => {
-        const blip = storeBlips[key]
-        expect(blip).toEqual({
-          ...blipInitialState,
-          beat: parseInt(key),
-          id: parseInt(key)
-        })
-      })
+    //   const storeBlips = store.getState().blips
+    //   _.keys(storeBlips).forEach((key) => {
+    //     const blip = storeBlips[key]
+    //     expect(blip).toEqual({
+    //       ...blipInitialState,
+    //       beat: parseInt(key),
+    //       id: parseInt(key)
+    //     })
+    //   })
 
-      // Restore uuid function
-      util.uuid = uuid
+    //   // Restore uuid function
+    //   util.uuid = uuid
 
-    })
+    // })
 
-    it("Child blips inherit the sample and color property.", () => {
-      const store = createStore(
-        combineReducers({
-          channels: channels.reducer,
-          blips: blips.reducer
-        }),
-        applyMiddleware(thunk)
-      )
-      store.dispatch(channels.actions.createChannel({
-        sample: 'hihat',
-        color: 'blue'
-      }))
-      expect(
-        _.values(store.getState().blips).every(
-          blip => blip.sample === 'hihat' && blip.color === 'blue'
-        )
-      ).toBe(true)
-    })
+    // it("Child blips inherit the sample and color property.", () => {
+    //   const store = createStore(
+    //     combineReducers({
+    //       channels: channels.reducer,
+    //       blips: blips.reducer
+    //     }),
+    //     applyMiddleware(thunk)
+    //   )
+    //   store.dispatch(channels.actions.createChannel({
+    //     sample: 'hihat',
+    //     color: 'blue'
+    //   }))
+    //   expect(
+    //     _.values(store.getState().blips).every(
+    //       blip => blip.sample === 'hihat' && blip.color === 'blue'
+    //     )
+    //   ).toBe(true)
+    // })
 
-    it("Child blips have their beat set.", () => {
-      const store = createStore(
-        combineReducers({
-          channels: channels.reducer,
-          blips: blips.reducer
-        }),
-        applyMiddleware(thunk)
-      )
-      store.dispatch(channels.actions.createChannel())
-      let range = _.range(16)
-      _.values(store.getState().blips).forEach((blip) => {
-        if (typeof blip.beat === 'number')
-          range = range.filter(i => i !== blip.beat)
-      })
-      expect(range).toEqual([])
-    })
+    // it("Child blips have their beat set.", () => {
+    //   const store = createStore(
+    //     combineReducers({
+    //       channels: channels.reducer,
+    //       blips: blips.reducer
+    //     }),
+    //     applyMiddleware(thunk)
+    //   )
+    //   store.dispatch(channels.actions.createChannel())
+    //   let range = _.range(16)
+    //   _.values(store.getState().blips).forEach((blip) => {
+    //     if (typeof blip.beat === 'number')
+    //       range = range.filter(i => i !== blip.beat)
+    //   })
+    //   expect(range).toEqual([])
+    // })
 
     it("Removes a channel.", () => {
       const stateBefore = {
@@ -188,6 +185,31 @@ describe("channel", () => {
       expect(
         channels.reducer(stateAfter, action)
       ).toEqual(stateBefore)
+    })
+
+    it("Sets a blip at a position.", () => {
+      const stateBefore = {
+        1: initialState
+      }
+      const action = channels.actions.setBlipAt(1, 2, 'foobar')
+      const stateAfter = {
+        1: {
+          ...initialState,
+          blips: [
+            undefined, undefined, 'foobar', undefined,
+            undefined, undefined, undefined, undefined,
+            undefined, undefined, undefined, undefined,
+            undefined, undefined, undefined, undefined
+          ]
+        }
+      }
+      expect(
+        channels.reducer(stateBefore, action)
+      ).toEqual(stateAfter)
+    })
+
+    describe("toggleBlipAtPosition", () => {
+
     })
 
   })
