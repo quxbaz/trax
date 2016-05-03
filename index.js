9073a21a44fa30a50d249674943b86db98502792
David Yeung <quxbaz@gmail.com>
2016-05-03 19:44:53 -0400

    Added basic scaffold of songs and editor modules

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import channels from './lib/channels'
 import blips from './lib/blips'
 import presets from './lib/presets'
 import mixables from './lib/mixables'
+import editor from './lib/editor'
+import songs from './lib/songs'
 import AudioService from './lib/AudioService'
 import Player from './lib/Player'
 
@@ -12,11 +14,14 @@ const reducers = {
   blips: blips.reducer,
   presets: presets.reducer,
   mixables: mixables.reducer,
+  editor: editor.reducer,
+  songs: songs.reducer,
 }
 
 export {
   reducers,
   sequencer, channels, blips,
   presets, mixables,
+  editor, songs,
   AudioService, Player
 }
---
+++ tests/tests/songs.js
@@ -0,0 +1,97 @@
+import expect from 'expect'
+import {songs} from 'trax'
+import {songInitialState} from 'trax/lib/songs/reducer'
+
+describe("songs", () => {
+
+  describe("reducer", () => {
+
+    it("Gets the initial state.", () => {
+      const stateBefore = undefined
+      const action = {}
+      const stateAfter = {}
+      expect(
+        songs.reducer(stateBefore, action)
+      ).toEqual(stateAfter)
+    })
+
+    it("Creates a song.", () => {
+      const stateBefore = undefined
+      const action = songs.actions.createSong({id: 'foo'})
+      const stateAfter = {
+        foo: {
+          ...songInitialState,
+          id: 'foo'
+        }
+      }
+      expect(
+        songs.reducer(stateBefore, action)
+      ).toEqual(stateAfter)
+    })
+
+    it("Removes a song.", () => {
+      const stateBefore = {
+        1: {id: 1},
+        2: {id: 2}
+      }
+      const action = songs.actions.removeSong(1)
+      const stateAfter = {
+        2: {id: 2}
+      }
+      expect(
+        songs.reducer(stateBefore, action)
+      ).toEqual(stateAfter)
+    })
+
+    it("Moves the cursor.", () => {
+
+    })
+
+    it("Sets a channel at a cell.", () => {
+
+    })
+
+    it("Empties a channel at a cell.", () => {
+
+    })
+
+  })
+
+  describe("selectors", () => {
+
+    describe("getAll()", () => {
+      it("Gets all songs.", () => {
+        const state = {
+          songs: {
+            1: {id: 1},
+            2: {id: 2}
+          }
+        }
+        const result = [
+          {id: 1},
+          {id: 2}
+        ]
+        expect(
+          songs.selectors.getAll(state)
+        ).toEqual(result)
+      })
+    })
+
+    describe("getById()", () => {
+      it("Gets a song by id.", () => {
+        const state = {
+          songs: {
+            1: {title: 'whoa'},
+            2: {}
+          }
+        }
+        const result = {title: 'whoa'}
+        expect(
+          songs.selectors.getById(1)(state)
+        ).toEqual(result)
+      })
+    })
+
+  })
+
+})
