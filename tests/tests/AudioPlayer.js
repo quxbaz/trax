cfc949595aa2a613daaee705be658f0fe9c5afcb
David Yeung <quxbaz@gmail.com>
2016-05-05 14:02:07 -0400

    Can now play songs

--- tests/tests/AudioPlayer.js
+++ tests/tests/AudioPlayer.js
@@ -1,7 +1,13 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import times from 'qux/lib/times'
 import {fetchSample} from '../test-util'
-import {reducers, channels, blips, presets, player, AudioService, AudioPlayer} from 'trax'
+import {
+  reducers, channels, blips,
+  presets, mixables, songs,
+  player, songPlayer,
+  AudioService, AudioPlayer
+} from 'trax'
 
 describe("AudioPlayer", () => {
 
@@ -25,8 +31,6 @@ describe("AudioPlayer", () => {
       combineReducers(reducers),
       applyMiddleware(thunk)
     )
-
-    const audioPlayer = new AudioPlayer({audioService, store, tickInterval: 50})
 
     store.dispatch(player.actions.createPlayer({playing: true}))
 
@@ -55,12 +59,99 @@ describe("AudioPlayer", () => {
       offset += 50
     }
 
+    const audioPlayer = new AudioPlayer({audioService, store, tickInterval: 50})
+
     setTimeout(() => {
       audioPlayer.timer.trigger('tick')
       audioPlayer.timer.trigger('tick')
       audioPlayer.timer.trigger('tick')
       audioPlayer.timer.trigger('tick')
     }, 200)
+
+  })
+
+  it("Plays a song.", () => {
+
+    const store = createStore(
+      combineReducers(reducers),
+      applyMiddleware(thunk)
+    )
+
+    store.dispatch(
+      presets.actions.createPreset({id: 'preset1', sample: 'hihat'})
+    )
+
+    store.dispatch(
+      mixables.actions.createMixable({id: 'mixable1', sample: 'hihat', rate: 1.5})
+    )
+
+    store.dispatch(
+      presets.actions.createPreset({id: 'preset2', sample: 'hihat', mixable: 'mixable1'})
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel1',
+        preset: 'preset1',
+      })
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel2',
+        preset: 'preset2',
+      })
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel3',
+        preset: 'preset1',
+      })
+    )
+
+    store.getState().channels['channel1'].blips.forEach((id, i) => {
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel1', i)
+      )
+    })
+
+    store.getState().channels['channel2'].blips.forEach((id, i) => {
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel2', i)
+      )
+    })
+
+    store.getState().channels['channel3'].blips.forEach((id, i) => {
+      if ((i % 2) === 0)
+        return
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel3', i)
+      )
+    })
+
+    const audioPlayer = new AudioPlayer({audioService, store, tickInterval: 100})
+
+    store.dispatch(
+      songPlayer.actions.createSongPlayer({beatDuration: 25})
+    )
+
+    store.dispatch(
+      songs.actions.createSong({
+        data: [
+          ['channel1'],
+          ['channel2'],
+          ['channel3'],
+          ['channel1', 'channel2'],
+        ]
+      })
+    )
+
+    setTimeout(() => {
+      audioPlayer.playSong(
+        songs.selectors.getAll(store.getState())[0]
+      )
+    }, 1000)
 
   })
 
