b14682c84d4d6fefb8d3357a6967f23eca7e77d3
David Yeung <quxbaz@gmail.com>
2016-05-05 14:20:41 -0400

    Stripped unnecessary song-player stuff

--- tests/tests/song-player.js
+++ tests/tests/song-player.js
@@ -27,45 +27,6 @@ describe("songPlayer", () => {
       ).toEqual(stateAfter)
     })
 
-    it("Plays.", () => {
-      const stateBefore = {playing: false}
-      const action = songPlayer.actions.play()
-      const stateAfter = {playing: true}
-      expect(
-        songPlayer.reducer(stateBefore, action)
-      ).toEqual(stateAfter)
-    })
-
-    it("Pauses.", () => {
-      const stateBefore = {playing: true}
-      const action = songPlayer.actions.pause()
-      const stateAfter = {playing: false}
-      expect(
-        songPlayer.reducer(stateBefore, action)
-      ).toEqual(stateAfter)
-    })
-
-    it("Toggles playing.", () => {
-      const stateBefore = {playing: false}
-      const action = songPlayer.actions.togglePlay()
-      const stateAfter = {playing: true}
-      expect(
-        songPlayer.reducer(stateBefore, action)
-      ).toEqual(stateAfter)
-      expect(
-        songPlayer.reducer(stateAfter, action)
-      ).toEqual(stateBefore)
-    })
-
-    it("Steps the beat forward.", () => {
-      const stateBefore = {beats: 16, currentBeat: 0}
-      const action = songPlayer.actions.tick()
-      const stateAfter = {beats: 16, currentBeat: 1}
-      expect(
-        songPlayer.reducer(stateBefore, action)
-      ).toEqual(stateAfter)
-    })
-
   })
 
 })
