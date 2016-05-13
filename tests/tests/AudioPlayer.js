3d4d20d3fed3411d29052e36ecec756bc5872534
David Yeung <quxbaz@gmail.com>
2016-05-13 16:34:39 -0400

    Can now play songs with blocks

--- tests/tests/AudioPlayer.js
+++ tests/tests/AudioPlayer.js
@@ -4,9 +4,10 @@ import times from 'qux/lib/times'
 import {fetchSample} from '../test-util'
 import {
   reducers, channels, blips,
-  blocks,
-  presets, mixables, songs,
-  player,
+  blocks, blockAdmin,
+  presets, mixables,
+  songs, songAdmin,
+  player, songPlayer,
   AudioService, AudioPlayer
 } from 'trax'
 
@@ -47,6 +48,10 @@ describe("AudioPlayer", () => {
     )
 
     store.dispatch(
+      blockAdmin.actions.setActiveBlock('block-id')
+    )
+
+    store.dispatch(
       channels.actions.createChannel({
         id: 1,
         sample: 'hihat',
@@ -75,6 +80,116 @@ describe("AudioPlayer", () => {
       audioPlayer.timer.trigger('tick')
       audioPlayer.timer.trigger('tick')
     }, 200)
+
+  })
+
+  it("Plays a song.", () => {
+
+    const store = createStore(
+      combineReducers(reducers),
+      applyMiddleware(thunk)
+    )
+
+    store.dispatch(
+      mixables.actions.createMixable({id: 'low', sample: 'hihat', rate: 0.7})
+    )
+
+    store.dispatch(
+      mixables.actions.createMixable({id: 'high', sample: 'hihat', rate: 1.5})
+    )
+
+    store.dispatch(
+      presets.actions.createPreset({id: 'preset', sample: 'hihat'})
+    )
+
+    store.dispatch(
+      presets.actions.createPreset({id: 'preset-low', sample: 'hihat', mixable: 'low'})
+    )
+
+    store.dispatch(
+      presets.actions.createPreset({id: 'preset-high', sample: 'hihat', mixable: 'high'})
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel',
+        preset: 'preset',
+      })
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel-low',
+        preset: 'preset-low',
+      })
+    )
+
+    store.dispatch(
+      channels.actions.createChannel({
+        id: 'channel-high',
+        preset: 'preset-high',
+      })
+    )
+
+    store.getState().channels['channel'].blips.forEach((id, i) => {
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel', i)
+      )
+    })
+
+    store.getState().channels['channel-low'].blips.forEach((id, i) => {
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel-low', i)
+      )
+    })
+
+    store.getState().channels['channel-high'].blips.forEach((id, i) => {
+      if ((i % 2) === 0)
+        return
+      store.dispatch(
+        channels.actions.toggleBlipAt('channel-high', i)
+      )
+    })
+
+    const audioPlayer = new AudioPlayer({audioService, store, tickInterval: 25})
+
+    store.dispatch(blocks.actions.createBlock({
+      id: 'block-1',
+      channels: ['channel']
+    }))
+
+    store.dispatch(blocks.actions.createBlock({
+      id: 'block-2',
+      channels: ['channel-low']
+    }))
+
+    store.dispatch(blocks.actions.createBlock({
+      id: 'block-3',
+      channels: ['channel-high']
+    }))
+
+    store.dispatch(blocks.actions.createBlock({
+      id: 'block-4',
+      channels: ['channel-low', 'channel-high']
+    }))
+
+    store.dispatch(blocks.actions.createBlock({
+      id: 'block-5',
+      channels: ['channel', 'channel-low', 'channel-high']
+    }))
+
+    store.dispatch(songs.actions.createSong({
+      id: 'song',
+      blocks: ['block-1', 'block-2', 'block-3', 'block-4', null, 'block-5'],
+    }))
+
+    store.dispatch(
+      songAdmin.actions.setCurrentSong('song')
+    )
+
+    setTimeout(() => {
+      audioPlayer.startSong()
+    }, 1000)
 
   })
 
