affb8b36beb42566432e34a95e634fff005ff508
David Yeung <quxbaz@gmail.com>
2016-05-13 21:41:56 -0400

    Removed test

--- tests/tests/song-admin.js
+++ tests/tests/song-admin.js
@@ -27,31 +27,6 @@ describe("song-admin", () => {
       ).toEqual(stateAfter)
     })
 
-    it("Sets the current song on creating the first song.", () => {
-      const store = createStore(
-        combineReducers({
-          songs: songs.reducer,
-          songAdmin: songAdmin.reducer,
-        }),
-        applyMiddleware(thunk)
-      )
-      store.dispatch(
-        songs.actions.createSong({id: 1})
-      )
-      expect(store.getState()).toEqual({
-        songAdmin: {
-          ...initialState,
-          currentSong: 1,
-        },
-        songs: {
-          1: {
-            ...songInitialState,
-            id: 1,
-          },
-        },
-      })
-    })
-
   })
 
   describe("selectors", () => {
